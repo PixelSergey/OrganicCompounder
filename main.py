1ded9c93d996b86b42056e0cf0d9707ae927c3b9
PixelSergey <sergey.ichtchenko@gmail.com>
2016-11-12 21:48:23 +0200

    Update OrganicCompounder to unfinished V2
    Will have a bunch of spicy features soon

--- main.py
+++ main.py
@@ -1,160 +1,73 @@
-#Hydrocarbon Wiki v1
+#Organic Compound Analyser V2
+#Features:
+#Branch-type, straight-chain and cyclo types are implemented to this version.
+#Also includes Alcohols
+#No proper imaging system...yet
+#Has a feature to scan for allotropes of a formula.
+#Cleaner code, easier to debug.
+#A proper menu
 
-#Functionality:
-#-Only does branch and straight-chain type hydrocarbons.
-#-Has diagram creation.
-#-Prefixes range from Meth- to Dec-.
-#-Cyclo types are not implemented.
+print("\t\tWelcome to the organic compounder V.2\n
+(c) 2016\n
+Please use organic molecules responsibly\n\n\n")
 
-#import time
+#Defined lists
+prefixes = ["meth", "eth", "prop", "but", "pent", "hex", "hept", "oct", "non", "dec", " "]
 
-print("\t\tWelcome to the organic compounder\n(c) 2016\nPlease use organic molecules responsibly\n\n\n")
-
-while True:
-    compound = input("Type in your hydrocarbon!\nThis program will define it!\n>>")
-    name = compound
-    compound = compound + " "
-    compound = list(compound.lower())
-    iterating_term = ""
-    prefix = ""
-    suffix = ""
-    prefix_list = ["meth", "eth", "prop", "but", "pent", "hex", "hept", "oct", "non", "dec"]
-    num_indicator = 0
-    branch_value = 0
-    carbon_value = 0
-    carbon_atom_value = 0
-    straight_chain = False
-    branch_type = False
-    #Iterating loop to define the sections of the hydrocarbon.
-    for x in compound:
-        if x == "-":
-                pass
-        try:
-            numberA = int(x)
-            num_indicator = num_indicator + 1
-            if num_indicator == 0:
-                carbon_value = carbon_value + numberA
-            elif num_indicator == 1:
-                branch_value = branch_value + carbon_value
-                carbon_value = numberA
-            else:
-                print("Malfunctioning...")
-        except:
+def scan(compound):
+    compound = list(compound)
+    moleculeType = ""
+    #Compound Iteration
+    #1. Test for normal Alk-
+    if compound[0].isdigit() == True:
+        moleculeType = "Alk"
+        iterating_term = ""
+        for x in compound:
             if x == "-":
+                pass
+            elif x.isdigit() == True:
                 pass
             else:
                 iterating_term = iterating_term + x
         for x in prefix_list:
-            if x + "yl" == iterating_term:
-                branch_type = True
-            elif x  + "ane"== iterating_term:
-                straight_chain = True
-                suffix = "ane"
-                prefix = x
-            elif x  + "ene"== iterating_term:
-                straight_chain = True
-                suffix = "ene"
-                prefix = x
-            elif x  + "yne"== iterating_term:
-                straight_chain = True
-                suffix = "yne"
-                prefix = x
-    #Idiot-proof.
-    if branch_type and straight_chain == False:
-        print("Malfunctioning...")
-        break
-    elif branch_type == True:
-        pass
-    elif straight_chain == True:
-        pass
-    elif suffix == "ene" or "yne" and carbon_value < 2:
-        print("This is not a valid hydrocarbon. Try again.")
-        print("(Error: -ene or -yne suffixes can't have less than 2 carbon atoms.)")
-        continue
-    elif carbon_value or branch_value == 0:
-        print("This is not a valid hydrocarbon. Try again.")
-        pring("(Error: bond position or branch position can't be 0)")
-        continue
+            if x  + "ane" == iterating_term:
+                moleculeType = moleculeType + "ane"
+            elif x  + "ene" == iterating_term:
+                moleculeType = moleculeType + "ene"
+            elif x  + "yne" == iterating_term:
+                moleculeType = moleculeType + "yne"
     else:
-        pass
-    #Interface
-    print("Hydrocarbon: {}".format(name))
-    print("Specs:")
-    if branch_type == True:
-        print("Branch-type hydrocarbon.")
-    elif straight_chain == True:
-        print("Straight-chain type hydrocarbon")
-    print("Note: '3-' in the display means a triple bond")
-    print("Hydrocarbon Structure:")
-    bond_vertical = " | "
-    double_bond_vertical = "| |"
-    triple_bond_vertical = "(|)"
-    bond_horizontal = "- -"
-    double_bond_horizontal = "= ="
-    triple_bond_horizontal = "3 -"
-    bond_diagonal_left = "//" #for cyclos later.
-    bond_diagonal_right = "\\\\"
-    space_filler = "   "
-    carbon_atom = " C "
-    hydrogen_atom = " H "
-    #setting up values
-    value_carbon = 0
-    value_hydrogen = 0
-    single_bond_value = 0
-    double_bond_value = 0
-    triple_bond_value = 0
-    #Prefix values
-    if prefix == "meth":
-        value_carbon = value_carbon + 1
-    elif prefix == "eth":
-        value_carbon = value_carbon + 2
-    elif prefix == "prop":
-        value_carbon = value_carbon + 3
-    elif prefix == "but":
-        value_carbon = value_carbon + 4
-    elif prefix == "pent":
-        value_carbon = value_carbon + 5
-    elif prefix == "hex":
-        value_carbon = value_carbon + 6
-    elif prefix == "hept":
-        value_carbon = value_carbon + 7
-    elif prefix == "oct":
-        value_carbon = value_carbon + 8
-    elif prefix == "non":
-        value_carbon = value_carbon + 9
-    elif prefix == "dec":
-        value_carbon = value_carbon + 10
-    #Detemining value_hydrogen
-    if suffix == "ane":
-        value_hydrogen = value_carbon * 2 + 2
-        single_bond_value = single_bond_value + value_hydrogen + value_carbon - 1
-    elif suffix == "ene":
-        value_hydrogen = value_carbon * 2
-        double_bond_value = double_bond_value + 1
-        single_bond_value = single_bond_value + value_hydrogen + value_carbon - 2
-    elif suffix == "yne":
-        value_hydrogen = value_carbon * 2 - 2
-        single_bond_value = single_bond_value + value_hydrogen + value_carbon - 2
-    #Display
-    layer1 = []
-    layer2 = []
-    layer3 = []
-    layer4 = []
-    layer5 = []
-    layer6 = []
-    layer7 = []
-    layer8 = []
-    layer9 = []
-    layerX = []
-    carbon_position_counter = 0
-    while True:
-        if carbon_value < 0 and suffix == "ene" and carbon_position_counter == carbon_value:
-            layer5.append(carbon_atom + double_bond_horizontal)
-            carbon_position_counter = carbon_position_counter + 1
-        elif carbon_value < 0 and suffix == "ene" and carbon_position_counter == carbon_value:
-            pass
-        else:
-            layer5.append(carbon_atom + bond_horizontal)
-            carbon_position_counter = carbon_position_counter + 1
-            layer5.append(carbon_atom)
+        print("Type not included in system yet...")
+        return
 
+def allotropes():
+    formula = input("Type in your formula, this program will find it's allotropes!\n>>")
+    display = input("Do you want an advanced view for each of your allotropes? (Y/n)")
+    if display.upper() == "Y" or display == "":
+        display = True
+    else:
+        display = False
+    #Make a list of parts    
+    formulalist = formula.split("C")
+    formulalist = formulalist[0].split("H") + formulalist
+    #Start with straight alkane
+    
+    
+    for compound in compounds:
+        if display:
+            scan(compound)
+        else:
+            print(compound)
+    
+#Menu
+while True:
+    sector = input("Which sector would you like to use?\n
+    \t[F]ind all the allotropes of a formula\n
+    \t[S]can an organic compound to find all about it")
+    #Navigation to each sector
+    if sector.lower() == "f":
+        allotropes()
+        
+    elif sector.lower() == "s":
+        scan(input("Type in your compound and this program will define it!\n>>") + " ")
+        
